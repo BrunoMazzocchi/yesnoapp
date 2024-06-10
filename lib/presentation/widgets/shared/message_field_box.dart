793e9caf116abece55815c35861702794383b787
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-06-10 09:33:03 -0600

    Added textformfield

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,23 +5,40 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final textController = TextEditingController();
+    // It will be used to focus on the text field and keep it
+    final FocusNode focusNode = FocusNode();
+
     return Row(
             children: [
               IconButton(
                 icon: const Icon(Icons.camera_alt),
                 onPressed: () {},
               ),
-              const Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Type a message',
+               Expanded(
+                child: TextFormField(
+                  focusNode: focusNode,
+                  keyboardType: TextInputType.text,
+                  controller: textController,
+                  decoration: const InputDecoration(
+                    hintText: 'Type a message and end it with "?"',
                     border: InputBorder.none,
                   ),
+                  onFieldSubmitted: (value) {
+                    debugPrint(value);
+                    textController.clear();
+                    focusNode.requestFocus();
+                  },
+                  onChanged: (value) => debugPrint(value),
                 ),
               ),
               IconButton(
                 icon: const Icon(Icons.send),
-                onPressed: () {},
+                onPressed: () {
+                  debugPrint(textController.text);
+                  textController.clear();
+                },
               ),
             ],
           );
